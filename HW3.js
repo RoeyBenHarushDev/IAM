d40c40882b2c93801a81e5f48aaedc44d6d2b3f5
Shahar <Shaharariel95@gmail.com>
2022-11-08 23:47:47 +0200

    Roey and Shahar did the first 2 ex in HW3, this is the try of ex3, will continue

--- HW3.js
+++ HW3.js
@@ -1,3 +1,27 @@
-console.log("Hi Roey");
+const http = require('http');
+const win = require('winston');
 
+const server = http.createServer();
+const logger = win.createLogger({
+    level: 'info',
+    format : win.format.json(),
+    defaultMeta: { service : "user-service"},
+    transports: [
+        new win.transports.File({ filename:'error.log', level: 'error' }),
+        new win.transports.File({ filename: 'combined.log'}),
+    ],
+});
+
+        server.on('request',(req,res) => {
+        res.writeHead(200, { "Content-Type": "text/html"}); //Status code in header
+        res.write("this is a debugging test!\n");
+        res.write("here is the breakpoint!\n");
+        logger.log({level:'info', message:`${req.method}`});
+        res.write("Hello world!");
+        res.end();
+    }).listen(8080);
+
+console.log(`listening on port ${8080}`);
+console.log("breakpoint");
+console.log(`listening on port ${8080}`);
 
